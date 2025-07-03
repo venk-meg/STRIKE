69b39998f2bd532ca80b29cf9f912836c3686eb2
riasonalker1906 <160419908+riasonalker1906@users.noreply.github.com>
2025-07-03 18:17:08 -0400

    CNN Version1 for Microprocessor Test Uploaded

---
+++ CNN Initial/gesture_cnn_v1.h
@@ -0,0 +1,9 @@
+#ifndef GESTURE_CNN_V1_H_
+#define GESTURE_CNN_V1_H_
+
+#include <cstdint>
+
+extern const unsigned char gesture_cnn_v1_tflite[];
+extern const unsigned int gesture_cnn_v1_tflite_len;
+
+#endif  // GESTURE_CNN_V1_H_
